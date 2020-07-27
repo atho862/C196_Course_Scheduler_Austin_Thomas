cfb635b6707697d77db5d25d14e2cd1072d67ba8
atho862 <54923852+atho862@users.noreply.github.com>
2020-07-26 23:11:04 -0500

    Add recyclerView reference to main activiter and implement the onChanged override

--- app/src/main/java/com/example/c196_course_scheduler_austin_thomas/MainActivity.java
+++ app/src/main/java/com/example/c196_course_scheduler_austin_thomas/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.example.c196_course_scheduler_austin_thomas.Adapters.TermAdapter;
 import com.example.c196_course_scheduler_austin_thomas.Entities.Term;
 import com.example.c196_course_scheduler_austin_thomas.ViewModels.TermViewModel;
 
@@ -21,12 +24,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        final TermAdapter adapter = new TermAdapter();
+        recyclerView.setAdapter(adapter);
+
         termViewModel = ViewModelProviders.of(this).get(TermViewModel.class);
         termViewModel.getAllTerms().observe(this, new Observer<List<Term>>() {
             @Override
             public void onChanged(List<Term> terms) {
-                //update RecyclerView
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                adapter.setTerms(terms);
             }
         });
     }
